cedc829d2695544843fc0005a55f0f347c8298c7
krausyd (∩｀-´)⊃━☆ﾟ.*･｡ﾟ <krausyd@hotmail.com>
2023-03-05 15:20:48 -0800

    Create Contact form

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import About from './components/About';
 import Nav from './components/Nav';
 import Gallery from './components/Gallery';
+import ContactForm from './components/Contact';
 
 function App() {
   const [categories] = useState([
@@ -29,6 +30,7 @@ function App() {
     <div>
       <Nav categories={categories} setCurrentCategory={setCurrentCategory} currentCategory={currentCategory} />
       <main>
+        <ContactForm />
         <Gallery currentCategory={currentCategory} />
         <About />
       </main>
