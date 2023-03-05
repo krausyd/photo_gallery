3e4e6f27f3f8d557fd31b86d0a7495d127888f74
krausyd (∩｀-´)⊃━☆ﾟ.*･｡ﾟ <krausyd@hotmail.com>
2023-03-05 14:49:18 -0800

    Create PhotoList component to render dynamically the photos based on the category

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import { capitalizeFirstLetter } from '../../utils/helpers';
-import photo from '../../assets/small/commercial/0.jpg';
+import PhotoList from '../PhotoList';
 
 function Gallery({currentCategory}) {
     return (
         <section>
             <h1>{capitalizeFirstLetter(currentCategory.name)}</h1>
             <p>{currentCategory.description}</p>
-            <div>
-                <img src={photo} alt="Commercial example" className="img-thumbnail mx-1" />
-            </div>
+            <PhotoList category={currentCategory.name} />
         </section>
     );
 };
